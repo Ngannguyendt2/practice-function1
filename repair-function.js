f4090b256e9afac2a38d5475bb7c4adf24fc3890
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-08-12 14:52:14 +0700

    up file repair code sample

---
+++ repair-function.js
@@ -0,0 +1,13 @@
+function addNumbers() {
+  let  firstNum = 4;
+   let secondNum = 8;
+  let  result = firstNum + secondNum;
+    return result;
+}
+
+function repairCodeSample() {
+   let result = 0;
+    alert(result);
+    result = addNumbers();
+    alert(result);
+}
